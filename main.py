bdd36a2a742fa4fb12227c973c9afb9d4b27a251
aswinzz <iit2016106@iiita.ac.in>
2018-10-09 03:45:49 +0530

    add wot ranking

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-from url import rank_url
+from url import rank_url,fetch_url
 from similarity import rank_similarity
+from wot import *
+KEY="d490ab2486ff4140b3ed73590b9908e1cbcf8933"
 
 dataset = pd.read_csv('text_emotion.csv')
 dataset = dataset.iloc[0:20,3].values
 
 print("URL RANKING : ",rank_url(dataset))
 print("SIMILARITY RANKING : ",rank_similarity(dataset))
+print("WOT RANKING : ",rank_wot(dataset))
