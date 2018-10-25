7db2c703fadaa26f667d228cc568b55e7e5f2c2c
aswanthkoleri <aswanth9495@gmail.com>
2018-10-25 14:56:21 +0530

    basic twitter app added

---
+++ TweetyApp/TweetyApp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('main/',include('main.urls'))
+]
+
+urlpatterns+=staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
