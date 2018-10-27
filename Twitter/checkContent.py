e587dedc567bf23e81f42f77b372a400e49fb6aa
aswinzz <iit2016106@iiita.ac.in>
2018-10-27 20:07:50 +0530

    change directory structure

---
+++ Twitter/checkContent.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import re, math
+from collections import Counter
+import urllib, sys, bs4
+from pyshorteners import Shortener
+try:
+	from .url import fetch_url
+except:
+	from url import fetch_url
+import os
+dirpath = os.getcwd()+'/Twitter/'
+
+class UrlExpand:
+	def __init__(self):
+		self.shortener = Shortener('Tinyurl')
+	def decodeURL(self,url):
+		try:
+			result = self.shortener.expand(url)
+			return result
+		except Exception as e:
+			return url
+
+
+
+def checkAdultContent(dataset):
+	adultContentDataset=None
+	try:
+		adultContentDataset = pd.read_csv(dirpath+'adultcontenturl.csv')
+	except:
+		adultContentDataset = pd.read_csv('adultcontenturl.csv')
+	adultContentDataset = adultContentDataset.iloc[0:3,0].values
+	urlExpand = UrlExpand()
+
+	if(len(dataset) == 0):
+		return 0
+	for data in dataset:
+		urls=fetch_url(data)
+		for url in urls:
+			print("checking url : "+url)
+			result = urlExpand.decodeURL(url)
+			if(result in adultContentDataset):
+				#returns 10, if adult content is present
+				return 10
+
+	#returns 0, if adult content isn't present
+	return 0
---
+++ Twitter/similarity.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import re, math
+from collections import Counter
+import urllib, sys, bs4
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+WORD = re.compile(r'\w+')
+
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+    
+    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+    
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+    
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+def cosine_sim(str1,str2):
+
+    vector1 = text_to_vector(str1)
+    vector2 = text_to_vector(str2)
+
+    cosine = get_cosine(vector1, vector2)
+
+    if(cosine>0.75):
+        return 1
+    return 0
+
+
+def rank_similarity(dataset):
+    counter=0
+    total=0
+    for i in range(0,len(dataset)-3):
+        total=total+1
+        hitinCluster = 0
+        for j in range(i-3,i+3):
+            if(j>0 and j<len(dataset)):
+                for k in range(j+1,i+4):
+                    if(j!=k and k>0 and k<len(dataset)):
+                        if(cosine_sim(dataset[j],dataset[k])):
+                            counter = counter+1
+                            hitinCluster = 1
+                            break
+            if(hitinCluster):
+                break
+            
+    if(total==0):
+        return 0
+        
+    similarity_rank=(float(counter)/total)*10
+    return similarity_rank
+
+# def rank_similarity(dataset):
+#     counter=0
+#     total=0
+#     for i in range(0,len(dataset)):
+#         total=total+1
+#         for j in range(i-3,i+3):
+#             if(j>0 and j<len(dataset)):
+#                 for k in range(i-3,i+3):
+#                     if(j!=k and k>0 and k<len(dataset)):
+#                         counter=counter+cosine_sim(dataset[j],dataset[k])
+#                         if(cosine_sim(dataset[j],dataset[k])):
+#                             print("yes : "+str(j)+" : "+str(k))
+#     print(counter)
+#     print(total)
+#     similarity_rank=(float(counter)/total)*10
+#     return similarity_rank
