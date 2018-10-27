c2063f2756644dee2e6420f8d09a161d48837584
druvalcr28 <bim2016501@iiita.ac.in>
2018-10-27 16:18:49 +0530

    add details page

--- TweetyApp/main/views.py
+++ TweetyApp/main/views.py
@@ -15,10 +15,21 @@ def main(request):
         if form.is_valid():
             form.save()
             details=singleuser(username)
+
+            if(details[6]==0):
+                details[6]="Non Anomalous"
+            elif(details[6]==1):
+                details[6]="Suspected"
+            else:
+                details[6]="Anomalous"
             print(details)
-            return HttpResponse("Url rank = "+str(details[0])+"\nSimilarity rank = "+str(details[1])+"\nWOT rank"+str(details[2])+"\nAdult content rank"+str(details[3])+"\nTime rank "+str(details[4])+ "Fal value = "+str(details[5]) + "type = "+str(details[6]) )
+            return render(request,'outputData.html',{ 'name':username,'urlRank':details[0], 'similarity':details[1], 'wot':details[2], 'adult':details[3], 'time':details[4], 'fal':details[5], 'type':details[6] })
+            #return HttpResponse("Url rank = "+str(details[0])+"\nSimilarity rank = "+str(details[1])+"\nWOT rank"+str(details[2])+"\nAdult content rank"+str(details[3])+"\nTime rank "+str(details[4])+ "Fal value = "+str(details[5]) + "type = "+str(details[6]) )
     else:
         form=DocumentForm()
     return render(request,'main.html',{
         'form' : form
     })
+
+def test1(request):
+    return render(request,'outputData.html')
