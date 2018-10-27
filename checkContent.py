848ff9b542211a65612b4e48f4fc97c065507ccf
aswinzz <iit2016106@iiita.ac.in>
2018-10-27 15:48:11 +0530

    fix twitter redirect url issue

--- checkContent.py
+++ checkContent.py
@@ -6,6 +6,7 @@ from collections import Counter
 import urllib, sys, bs4
 from pyshorteners import Shortener
 from url import fetch_url
+import requests
 
 class UrlExpand:
 	def __init__(self):
@@ -29,11 +30,13 @@ def checkAdultContent(dataset):
 	for data in dataset:
 		urls=fetch_url(data)
 		for url in urls:
+			r=requests.get(url)
+			url=r.url
 			print("checking url : "+url)
 			result = urlExpand.decodeURL(url)
 			if(result in adultContentDataset):
 				#returns 10, if adult content is present
 				return 10
 
-	#returns 0, if adult content isn't present	
+	#returns 0, if adult content isn't present
 	return 0
